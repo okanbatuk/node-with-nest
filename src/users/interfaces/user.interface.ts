cc718c3934405901f1f651b02b59cba0af54ec45
myrn <okanbatuk@protonmail.com>
2023-04-03 02:01:42 +0300

    Applied serialization to sensitive data

--- src/users/interfaces/user.interface.ts
+++ src/users/interfaces/user.interface.ts
@@ -1,7 +1,24 @@
+import { Exclude } from 'class-transformer';
+
 export interface User {
   id: number;
   username: string;
   email: string;
   password: string;
   age: number;
+}
+
+export class SerializedUser implements User {
+  @Exclude()
+  id: number;
+  @Exclude()
+  password: string;
+
+  username: string;
+  email: string;
+  age: number;
+
+  constructor(partial: Partial<SerializedUser>) {
+    Object.assign(this, partial);
+  }
 }
--- src/users/interfaces/users.controller.interface.ts
+++ src/users/interfaces/users.controller.interface.ts
@@ -1,10 +1,10 @@
 import { Response } from 'express';
 import { CreateUserDto } from '../dtos/CreateUser.dto';
-import { User } from './user.interface';
+import { SerializedUser } from './user.interface';
 
 export interface IUsersController {
-  getUsers: (sortBy: boolean) => Promise<User[]>;
+  getUsers: (sortBy: boolean, res: Response) => Promise<SerializedUser[]>;
   createUser: (userPayload: CreateUserDto, res: Response) => Promise<Response>;
-  getUserById: (userId: number, res: Response) => Promise<Response>;
+  getUserById: (userId: number, res: Response) => Promise<SerializedUser[]>;
   deleteUserById: (userId: number, res: Response) => Promise<Response>;
 }
